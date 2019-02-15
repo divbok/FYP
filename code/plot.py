90ae2d37aeac30318ade5260ebddf07ab5995850
divbok <dbokadia@gmail.com>
2019-02-15 15:40:13 +0530

    random

---
+++ code/plot.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+matplotlib.use('agg')
+#results = pd.read_csv("results/decisiontreePRNR_SPR",names =['noofperm','accuracy','precision','recall','fscore'])
+#n_perm = permRanks.shape[0]
+
+
+#sample_data_table = FF.create_table(results)
+#py.iplot(sample_data_table, filename='sample-data-table')
+#plt.plot(results[noofperm], results[accuracy], 'ro')
+#plt.axis([0, 6, 0, 20)
+#plt.show()
