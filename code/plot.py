580fb8a596ac2ee65bdff425c930d4d3c8f76431
Akash.A <akashabi00@gmail.com>
2019-02-15 16:04:17 +0530

    Added plot.py

--- code/plot.py
+++ code/plot.py
@@ -5,13 +5,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-matplotlib.use('agg')
-#results = pd.read_csv("results/decisiontreePRNR_SPR",names =['noofperm','accuracy','precision','recall','fscore'])
+
+results = pd.read_csv("results/decisiontreePRNR_SPR",names =['noofperm','accuracy','precision','recall','fscore'],sep='\s+')
 #n_perm = permRanks.shape[0]
+
+noofperms = [x for x in results['noofperm']]
+accuracy  = [x for x in results['accuracy']]
+
+# noofperms = [1,2,3,4]
+# accuracy = [4,3,2,1]
+
+print(noofperms)
+print(accuracy)
 
 
 #sample_data_table = FF.create_table(results)
 #py.iplot(sample_data_table, filename='sample-data-table')
-#plt.plot(results[noofperm], results[accuracy], 'ro')
-#plt.axis([0, 6, 0, 20)
-#plt.show()
+plt.plot(results['noofperm'].tolist(),results['accuracy'].tolist())
+plt.plot(results['noofperm'].tolist(),results['precision'].tolist())
+plt.plot(results['noofperm'].tolist(),results['recall'].tolist())
+plt.plot(results['noofperm'].tolist(),results['fscore'].tolist())
+
+plt.legend(['Accuracy','Precision','Recall','F-Score'],loc="upper left")
+
+plt.axis([0, 110, 0.6, 1])
+plt.title("DecisionTree PRNR+SPR")
+plt.ylabel('Metrics')
+plt.xlabel('Number of Permissions')
+plt.show()
+fig = plt.figure()
+fig.savefig("plots/decisiontreePRNR_SPR.png",bbox_inches="tight")
+
